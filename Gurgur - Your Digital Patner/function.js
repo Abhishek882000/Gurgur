969e5090359d15f7a57a3838ee92f31e65a24fca
Abhishek882000 <jhaabhishek318@gmail.com>
2020-09-11 18:18:38 +0530

    Added

---
+++ Gurgur - Your Digital Patner/function.js
@@ -0,0 +1,43 @@
+
+/** code by webdevtrick ( https://webdevtrick.com ) **/
+var accordions = document.getElementsByClassName("accordion");
+
+for (var i = 0; i < accordions.length; i++) {
+    accordions[i].onclick = function () {
+        this.classList.toggle('is-open');
+
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            // accordion is currently open, so close it
+            content.style.maxHeight = null;
+        } else {
+            // accordion is currently closed, so open it
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    }
+}
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
